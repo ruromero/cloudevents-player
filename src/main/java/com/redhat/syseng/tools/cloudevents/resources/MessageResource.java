28361467a7eb46b32e53a8172ed321e2df72b35e
Ruben Romero Montes <rromerom@redhat.com>
2023-01-25 22:47:49 +0100

    [#38] Support structured and binary content modes (#39)
    * Use of quarkus-reactive libraries
    * Add CORS configuration
    * Bump cloudevents and quarkus versions
    * Allow the loopback endpoint to be configured based on quarkus.http.port
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    Signed-off-by: ruromero <rromerom@redhat.com>

--- src/main/java/com/redhat/syseng/tools/cloudevents/resources/MessageResource.java
+++ src/main/java/com/redhat/syseng/tools/cloudevents/resources/MessageResource.java
@@ -2,8 +2,6 @@ package com.redhat.syseng.tools.cloudevents.resources;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
@@ -17,11 +15,11 @@ import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.reactive.ResponseStatus;
+import org.jboss.resteasy.reactive.RestHeader;
+import org.jboss.resteasy.reactive.RestQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,9 +29,10 @@ import com.redhat.syseng.tools.cloudevents.model.Message;
 import com.redhat.syseng.tools.cloudevents.service.MessageService;
 
 import io.cloudevents.CloudEvent;
+import io.cloudevents.jackson.JsonFormat;
 
 @Path("/messages")
-@Consumes(MediaType.APPLICATION_JSON)
+@Consumes({MediaType.APPLICATION_JSON, JsonFormat.CONTENT_TYPE})
 @Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
@@ -49,35 +48,31 @@ public class MessageResource {
     ObjectMapper mapper;
 
     @GET
-    public CompletionStage<List<Message>> list(@QueryParam("page") @DefaultValue("0") Integer page,
-                                               @QueryParam("size") @DefaultValue("10") Integer size) {
-        return CompletableFuture.supplyAsync(() -> msgService.list(page, size));
+    public List<Message> list(@RestQuery @DefaultValue("0") Integer page,
+                                               @RestQuery @DefaultValue("10") Integer size) {
+        return msgService.list(page, size);
     }
 
     @POST
     @ResponseStatus(202)
-    public CompletableFuture<Void> sendEvent(CloudEvent object) {
-        return CompletableFuture.supplyAsync(() -> {
-            Set<ConstraintViolation<CloudEvent>> violations = validator.validate(object);
-            if (!violations.isEmpty()) {
-                LOGGER.debug("Validation error {}", violations);
-                try {
-                    throw new BadRequestException(mapper.writeValueAsString(violations));
-                } catch (JsonProcessingException e) {
-                    throw new InternalServerErrorException(e);
-                }
+    public void sendEvent(CloudEvent object, @RestHeader String contentType) {
+        Set<ConstraintViolation<CloudEvent>> violations = validator.validate(object);
+        if (!violations.isEmpty()) {
+            LOGGER.debug("Validation error {}", violations);
+            try {
+                throw new BadRequestException(mapper.writeValueAsString(violations));
+            } catch (JsonProcessingException e) {
+                throw new InternalServerErrorException(e);
             }
-            LOGGER.debug("New event to send: {}", object);
-            msgService.send(object);
-            return null;
-        });
+        }
+        LOGGER.debug("New event to send: {}", object);
+        msgService.send(object, JsonFormat.CONTENT_TYPE.equalsIgnoreCase(contentType));
     }
+    
 
     @DELETE
-    public CompletionStage<Response> clear() {
-        return CompletableFuture.supplyAsync(() -> {
-            msgService.clear();
-            return Response.accepted().build();
-        });
+    @ResponseStatus(202)
+    public void clear() {
+        msgService.clear();
     }
 }
--- src/main/java/com/redhat/syseng/tools/cloudevents/service/BrokerService.java
+++ src/main/java/com/redhat/syseng/tools/cloudevents/service/BrokerService.java
@@ -1,19 +1,30 @@
 package com.redhat.syseng.tools.cloudevents.service;
 
-import java.util.concurrent.CompletionStage;
-
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestResponse;
 
 import io.cloudevents.CloudEvent;
+import io.cloudevents.jackson.JsonFormat;
 
 @Path("/")
+@RegisterClientHeaders
+@RegisterRestClient
 public interface BrokerService {
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    CompletionStage<Response> send(CloudEvent payload);
+    RestResponse<Void> sendBinary(CloudEvent payload);
+
+    @POST
+    @Consumes(JsonFormat.CONTENT_TYPE)
+    @Produces(MediaType.APPLICATION_JSON)
+    RestResponse<Void> sendStructured(CloudEvent payload);
 }
